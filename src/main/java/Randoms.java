76815d9cb322e4a575d365be6458080fa6a30063
Popova Olesya <popovaolesya87@gmail.com>
2022-05-11 00:09:07 +0300

    add files

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,41 @@
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    protected int diff;
+    protected int min;
+
+    public Randoms(int min, int max) {
+        random = new Random(max);
+        this.min = min;
+        this.diff = max - min;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return random.nextInt(diff + 1) + min;
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Integer> spliterator() {
+        return Iterable.super.spliterator();
+    }
+}
